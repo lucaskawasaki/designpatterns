08564f646cbf8259066c5dd6126396baea72b4fc
lucaskawasaki <kawasaki.ryuji8@gmail.com>
2018-02-24 00:05:45 -0300

    adicionando codigo do SimUDuck

---
+++ 01capitulo/Strategy/SimUDuck/src/model/FlyBehavior.java
@@ -0,0 +1,6 @@
+package model;
+
+public interface FlyBehavior {
+
+	public void fly();
+}
---
+++ 01capitulo/Strategy/SimUDuck/src/model/FlyNoWay.java
@@ -0,0 +1,10 @@
+package model;
+
+public class FlyNoWay implements FlyBehavior {
+
+	@Override
+	public void fly() {
+		System.out.println("I can't fly!");
+	}
+
+}
